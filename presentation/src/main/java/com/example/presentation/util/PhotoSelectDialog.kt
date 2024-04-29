c4bb9d4a32d9ed0ba9b656a1a5604f7af5fbd2cd
CHOI97 <chlwjrh@gmail.com>
2024-04-29 15:59:03 +0900

    feat: 사진 선택 다이얼로그 수정

---
+++ presentation/src/main/java/com/example/presentation/util/PhotoSelectDialog.kt
@@ -0,0 +1,11 @@
+package com.example.presentation.util
+
+import com.example.presentation.R
+import com.example.presentation.base.BaseDialogFragment
+import com.example.presentation.databinding.DialogPhotoSelectBinding
+
+class PhotoSelectDialog: BaseDialogFragment<DialogPhotoSelectBinding>(R.layout.dialog_photo_select) {
+    override fun init() {
+
+    }
+}
--- presentation/src/main/java/com/example/presentation/view/login/OnboardingActivity.kt
+++ presentation/src/main/java/com/example/presentation/view/login/OnboardingActivity.kt
@@ -3,12 +3,20 @@ package com.example.presentation.view.login
 import com.example.presentation.R
 import com.example.presentation.base.BaseActivity
 import com.example.presentation.databinding.ActivityOnboardingBinding
+import com.example.presentation.util.PhotoSelectDialog
 
 class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
     override fun initViewModel() {
     }
 
     override fun init() {
+        setOnClickProfile()
+    }
+
+    private fun setOnClickProfile() {
+        binding.civOnboardingProfile.onThrottleClick {
+            PhotoSelectDialog().show(supportFragmentManager, "PermissionDialog")
+        }
     }
 
 }
