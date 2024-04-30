809c54856c70649d0a0db290a993dfaf63e326c0
syjeuion <105288129+syjeuion@users.noreply.github.com>
2024-04-30 23:24:43 +0900

    [BVVER24-109] feat: recyclerView item profile영역 작업

---
+++ presentation/src/main/java/com/example/presentation/view/mypage/MypageMyReviewFragment.kt
@@ -0,0 +1,11 @@
+package com.example.presentation.view.mypage
+
+import com.example.presentation.R
+import com.example.presentation.base.BaseFragment
+import com.example.presentation.databinding.FragmentMypageMyReviewBinding
+
+class MypageMyReviewFragment : BaseFragment<FragmentMypageMyReviewBinding>(R.layout.fragment_mypage_my_review) {
+    override fun init() {
+
+    }
+}
