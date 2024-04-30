2408d8d9873c45848ee03a97f86796c4ab7aefdd
syjeuion <105288129+syjeuion@users.noreply.github.com>
2024-04-30 18:26:38 +0900

    [BVVER24-107] feat: toolbar, profile img UI 작업

---
+++ presentation/src/main/java/com/example/presentation/view/mypage/MypageEditProfileFragment.kt
@@ -0,0 +1,11 @@
+package com.example.presentation.view.mypage
+
+import com.example.presentation.R
+import com.example.presentation.base.BaseFragment
+import com.example.presentation.databinding.FragmentMypageEditProfileBinding
+
+class MypageEditProfileFragment : BaseFragment<FragmentMypageEditProfileBinding>(R.layout.fragment_mypage_edit_profile) {
+    override fun init() {
+
+    }
+}
