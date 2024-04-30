b9dab99edc82140d95264c55edad35d1c8648f20
CHOI97 <chlwjrh@gmail.com>
2024-04-30 09:00:46 +0900

    feat: 채식 성향 Dropdown 추가 및 UI 수정

--- presentation/src/main/java/com/example/presentation/view/login/OnboardingActivity.kt
+++ presentation/src/main/java/com/example/presentation/view/login/OnboardingActivity.kt
@@ -1,16 +1,33 @@
 package com.example.presentation.view.login
 
+import android.widget.ArrayAdapter
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.example.presentation.R
 import com.example.presentation.base.BaseActivity
 import com.example.presentation.databinding.ActivityOnboardingBinding
 import com.example.presentation.util.PhotoSelectDialog
 
 class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
+    val items = arrayOf("알고 있지 않아요","비건","락토 베지테리언","오보 베지테리언","락토 오보 베지테리언","페스코 베지테리언","폴로 베지테리언","플렉시테리언")
     override fun initViewModel() {
+        // 뷰모델 만들기
+        // 닉네임 옵저빙
+        // 레벨 옵저빙
+        // 유효성검사
+        // 검사에 따른 버튼활성화
     }
 
     override fun init() {
         setOnClickProfile()
+
+        val adapter = ArrayAdapter(this, R.layout.item_dropdown, items)
+
+        binding.actvOnboardingEditDropdown.setAdapter(adapter)
+
+        binding.actvOnboardingEditDropdown.setOnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position).toString()
+            // 선택된 항목에 대한 처리 추가
+        }
     }
 
     private fun setOnClickProfile() {
