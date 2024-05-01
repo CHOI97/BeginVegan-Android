20e8652a858157674c1bf47443efb40c67c674c0
syjeuion <105288129+syjeuion@users.noreply.github.com>
2024-05-02 00:09:30 +0900

    [BVVER24-117] feat: 나의 매거진 UI 작업

---
+++ presentation/src/main/java/com/example/presentation/view/mypage/MypageMyMagazineFragment.kt
@@ -0,0 +1,11 @@
+package com.example.presentation.view.mypage
+
+import com.example.presentation.R
+import com.example.presentation.base.BaseFragment
+import com.example.presentation.databinding.FragmentMypageMyMagazineBinding
+
+class MypageMyMagazineFragment : BaseFragment<FragmentMypageMyMagazineBinding>(R.layout.fragment_mypage_my_magazine) {
+    override fun init() {
+
+    }
+}
