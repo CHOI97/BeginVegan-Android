b76321c584c41afb59c3738c01e287a4619a3578
syjeuion <105288129+syjeuion@users.noreply.github.com>
2024-05-02 04:33:35 +0900

    [BVVER24-118] feat: 설정 화면 UI 작업

---
+++ presentation/src/main/java/com/example/presentation/view/mypage/MypageSettingFragment.kt
@@ -0,0 +1,11 @@
+package com.example.presentation.view.mypage
+
+import com.example.presentation.R
+import com.example.presentation.base.BaseFragment
+import com.example.presentation.databinding.FragmentMypageSettingBinding
+
+class MypageSettingFragment : BaseFragment<FragmentMypageSettingBinding>(R.layout.fragment_mypage_setting) {
+    override fun init() {
+
+    }
+}
