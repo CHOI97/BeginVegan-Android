6e592949e9955db3b4ca781957187888fbafaab8
syjeuion <105288129+syjeuion@users.noreply.github.com>
2024-05-01 21:54:50 +0900

    [BVVER24-112] feat: 나의 식당 UI 작업

---
+++ presentation/src/main/java/com/example/presentation/view/mypage/MypageMyRestaurantFragment.kt
@@ -0,0 +1,16 @@
+package com.example.presentation.view.mypage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.presentation.R
+import com.example.presentation.base.BaseFragment
+import com.example.presentation.databinding.FragmentMypageMyRestaurantBinding
+
+class MypageMyRestaurantFragment : BaseFragment<FragmentMypageMyRestaurantBinding>(R.layout.fragment_mypage_my_restaurant) {
+    override fun init() {
+
+    }
+}
