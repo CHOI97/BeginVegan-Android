ef45d6be5758d961183536e45203bdfd53d69ec7
syjeuion <105288129+syjeuion@users.noreply.github.com>
2024-05-02 23:55:26 +0900

    [BVVER24-119] feat: 테스트 화면 UI 작업

---
+++ presentation/src/main/java/com/example/presentation/view/home/VeganTestFragment.kt
@@ -0,0 +1,11 @@
+package com.example.presentation.view.home
+
+import com.example.presentation.R
+import com.example.presentation.base.BaseFragment
+import com.example.presentation.databinding.FragmentVeganTestBinding
+
+class VeganTestFragment : BaseFragment<FragmentVeganTestBinding>(R.layout.fragment_vegan_test) {
+    override fun init() {
+
+    }
+}
