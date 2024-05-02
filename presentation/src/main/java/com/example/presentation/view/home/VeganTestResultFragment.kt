fc724d11a40b3ae5c28a0f983915dff8182ed218
syjeuion <105288129+syjeuion@users.noreply.github.com>
2024-05-03 03:41:11 +0900

    [BVVER24-119] feat: 비건테스트 결과화면 UI

---
+++ presentation/src/main/java/com/example/presentation/view/home/VeganTestResultFragment.kt
@@ -0,0 +1,11 @@
+package com.example.presentation.view.home
+
+import com.example.presentation.R
+import com.example.presentation.base.BaseFragment
+import com.example.presentation.databinding.FragmentVeganTestResultBinding
+
+class VeganTestResultFragment : BaseFragment<FragmentVeganTestResultBinding>(R.layout.fragment_vegan_test_result) {
+    override fun init() {
+
+    }
+}
