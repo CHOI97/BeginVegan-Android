afc33a8399da4fe96114ff471bbe21b5cb8ad699
syjeuion <105288129+syjeuion@users.noreply.github.com>
2024-05-05 23:36:13 +0900

    [BVVER24-123] feat: 나를위한레시피 tooltip dialog

--- presentation/src/main/java/com/example/presentation/view/tips/TipsRecipeFragment.kt
+++ presentation/src/main/java/com/example/presentation/view/tips/TipsRecipeFragment.kt
@@ -1,11 +1,30 @@
 package com.example.presentation.view.tips
 
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.presentation.R
 import com.example.presentation.base.BaseFragment
 import com.example.presentation.databinding.FragmentTipsRecipeBinding
+import com.example.presentation.view.mypage.MypageDeleteAccountDialog
 
 class TipsRecipeFragment : BaseFragment<FragmentTipsRecipeBinding>(R.layout.fragment_tips_recipe) {
+    private lateinit var recipeRvAdapter: TipsRecipeRvAdapter
     override fun init() {
+        recipeRvAdapter = TipsRecipeRvAdapter(requireContext())
+        binding.rvRecipe.adapter = recipeRvAdapter
+        binding.rvRecipe.layoutManager = LinearLayoutManager(this.context)
 
+        recipeRvAdapter.setOnItemClickListener(object :TipsRecipeRvAdapter.OnItemClickListener{
+            override fun onItemClick() {
+
+            }
+        })
+
+        binding.ibTooltipRecipeForMe.setOnClickListener {
+            openDialogRecipeForMe()
+        }
+    }
+
+    private fun openDialogRecipeForMe(){
+        TipsRecipeForMeDialog().show(childFragmentManager, "TipsRecipeForMe")
     }
 }
