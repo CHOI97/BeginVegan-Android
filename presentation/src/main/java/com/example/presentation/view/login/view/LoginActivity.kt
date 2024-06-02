32086fd42a7591fb379bf2e336db33ad64d10a33
CHOI97 <chlwjrh@gmail.com>
2024-06-03 04:44:05 +0900

    feat: network 에러 핸들링 및 임시 로그인 object, 로직 구현

--- presentation/src/main/java/com/example/presentation/view/login/view/LoginActivity.kt
+++ presentation/src/main/java/com/example/presentation/view/login/view/LoginActivity.kt
@@ -6,6 +6,7 @@ import com.example.presentation.R
 import com.example.presentation.base.BaseActivity
 import com.example.presentation.databinding.ActivityLoginBinding
 import com.example.presentation.view.login.viewModel.LoginViewModel
+import com.example.presentation.view.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 //
@@ -279,6 +280,19 @@ import dagger.hilt.android.AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
     private val viewModel: LoginViewModel by viewModels()
     override fun initViewModel() {
+        viewModel.loginState.observe(this){
+            when(it){
+                true -> {
+                    showToast("Test: 로그인 성공")
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                false ->{
+                    showToast("Test: 로그인 에러")
+                }
+            }
+        }
+
 
     }
 
@@ -289,11 +303,8 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
 
     private fun setOnClickLogin() {
         binding.btnLoginKakao.setOnClickListener {
+            /** 임시 테스트 계정 **/
             viewModel.signIn("test5@test.com","1")
-            val intent = Intent(this, OnboardingActivity::class.java)
-            startActivity(intent)
         }
-
     }
-
 }
--- presentation/src/main/java/com/example/presentation/view/login/viewModel/LoginViewModel.kt
+++ presentation/src/main/java/com/example/presentation/view/login/viewModel/LoginViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.viewModelScope
 import com.example.domain.model.AuthToken
 import com.example.domain.useCase.auth.SignInUseCase
 import com.example.domain.useCase.auth.SignUpUseCase
+import com.example.presentation.auth.User
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import timber.log.Timber
@@ -25,13 +26,21 @@ class LoginViewModel @Inject constructor(
         }
     }
 
-    private val _authToken = MutableLiveData<AuthToken>()
-    val authToken: LiveData<AuthToken> = _authToken
+    private val _loginState = MutableLiveData<Boolean>()
+    val loginState: LiveData<Boolean> = _loginState
+
+    private val _signupState = MutableLiveData<Result<Unit>>()
+    val signupState: LiveData<Result<Unit>> = _signupState
 
     fun signIn(email: String, providerId: String) {
         viewModelScope.launch {
-            val result = signInUseCase.invoke(email, providerId)
-            Timber.d("$result")
+            signInUseCase.invoke(email, providerId).onSuccess {
+                User.accessToken = it.accessToken
+                User.refreshToken = it.refreshToken
+                _loginState.postValue(true)
+            }.onFailure {
+                _loginState.postValue(false)
+            }
         }
     }
 }
