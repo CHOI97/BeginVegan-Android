0b66c34714b03d4c2b38495bf7056ff1951e86f8
syjeuion <105288129+syjeuion@users.noreply.github.com>
2024-07-06 17:33:34 +0900

    [BVVER24-133] chore: build

--- presentation/src/main/java/com/example/presentation/config/drawer/DrawerControllerModule.kt
+++ presentation/src/main/java/com/example/presentation/config/drawer/DrawerControllerModule.kt
@@ -1,6 +1,7 @@
 package com.example.presentation.config.drawer
 
 import android.app.Activity
+import android.view.View
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.presentation.R
@@ -9,6 +10,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import timber.log.Timber
 
 @Module
 @InstallIn(ActivityComponent::class)
@@ -16,13 +18,23 @@ object DrawerControllerModule {
     @Provides
     fun provideDrawerHandler(activity: Activity): DrawerController = object : DrawerController {
         override fun openDrawer() {
+            Timber.d("open Drawer")
             val drawerLayout: DrawerLayout = activity.findViewById(R.id.dl_drawer)
             drawerLayout.openDrawer(GravityCompat.END)
+
+//            val preventMissTouch: View = activity.findViewById(R.id.v_prevent_miss_touch)
+//            preventMissTouch.visibility = View.VISIBLE
+//            preventMissTouch.setOnClickListener {
+//                closeDrawer()
+//            }
         }
         override fun closeDrawer() {
             val drawerLayout: DrawerLayout = activity.findViewById(R.id.dl_drawer)
             drawerLayout.closeDrawer(GravityCompat.END)
             drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+
+//            val preventMissTouch: View = activity.findViewById(R.id.v_prevent_miss_touch)
+//            preventMissTouch.visibility = View.GONE
         }
     }
 }
--- presentation/src/main/java/com/example/presentation/view/main/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/view/main/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.presentation.view.main
 
+import android.content.Context
+import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
+import androidx.core.view.GravityCompat
 import androidx.core.view.isVisible
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.viewModels
@@ -21,6 +24,7 @@ import com.example.presentation.view.notification.view.NotificationDrawerFragmen
 import com.example.presentation.view.notification.viewModel.NotificationViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 
@@ -33,7 +37,9 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun initViewModel() {
     }
     override fun init() {
+        Timber.d("Main Activity init")
         setDrawer()
+        backButton()
     }
 
     private fun setDrawer(){
@@ -91,5 +97,20 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         val oldRvAdapter = NotificationReadRvAdapter(list, this)
         readRecyclerView.adapter = oldRvAdapter
         readRecyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
+    //back button 동작 제어
+    private fun backButton(){
+        onBackPressedDispatcher.addCallback(this, object: OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                Timber.d("isDrawerOpen: ${binding.dlDrawer.isDrawerOpen(GravityCompat.END)}")
+                if(binding.dlDrawer.isDrawerOpen(GravityCompat.END)){
+                    drawerController.closeDrawer()
+                }else{
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
     }
 }
